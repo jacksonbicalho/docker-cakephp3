a3ee4e89c32bb1f66b607ac1480723a3d9b6a710
Jackson Bicalho <jacksonbicalho@gmail.com>
2018-09-25 11:28:10 -0300

    Inserindo arquivo para criação de schema

---
+++ src/database/psql_dump.sql
@@ -0,0 +1,27 @@
+-- Database generated with pgModeler (PostgreSQL Database Modeler).
+-- pgModeler  version: 0.9.1-beta
+-- PostgreSQL version: 10.0
+-- Project Site: pgmodeler.com.br
+-- Model Author: ---
+
+
+-- Database creation must be done outside an multicommand file.
+-- These commands were put in this file only for convenience.
+-- -- object: new_database | type: DATABASE --
+-- -- DROP DATABASE IF EXISTS new_database;
+-- CREATE DATABASE new_database
+-- ;
+-- -- ddl-end --
+--
+
+-- object: ovitrampas | type: SCHEMA --
+-- DROP SCHEMA IF EXISTS ovitrampas CASCADE;
+CREATE SCHEMA developer;
+-- ddl-end --
+ALTER SCHEMA developer OWNER TO developer;
+-- ddl-end --
+
+SET search_path TO developer;
+-- ddl-end --
+
+
